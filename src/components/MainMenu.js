f58d7cce79a50da3655827c7933131f1655c1cb7
Jesse <ronda.jesse@gmail.com>
2024-01-16 16:41:43 -0500

    styled and added functionality to rules modal

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import RulesModal from './RulesModal';
 import logo from '../starter-code/images/logo.svg';
 import pvpImg from '../starter-code/images/player-vs-player.svg';
 
 const MainMenu = ({ gameStartStop }) => {
+  const [showRules, setShowRules] = useState(false);
+
+  const rulesClick = (e) => {
+    setShowRules(!showRules);
+  }
+
   return (
-    <div id="home-cont">
-      <div id="home-cont-sq">
-        <div id="home-sel-cont">
-          <div id="home-img">
-            <img
-              id="home-img"
-              src={logo}
-              alt="logo"
-            />
-          </div>
-          <div id="home-btn-cont">
-            <button id="pvp-btn" className="big-yellow-btn heading-s" onClick={gameStartStop}>
-              <span>player vs player</span>
-              <img 
-                src={pvpImg}
-                alt="Player vs player"
-              />
-              </button>
-            <button id="rules-btn" className="big-white-btn heading-s">game rules</button>
+    <Fragment>
+      {showRules ? (
+        <RulesModal rulesClick={rulesClick}/>
+      ) : (
+        <div id="home-cont">
+          <div id="home-cont-sq">
+            <div id="home-sel-cont">
+              <div id="home-img">
+                <img
+                  id="home-img"
+                  src={logo}
+                  alt="logo"
+                />
+              </div>
+              <div id="home-btn-cont">
+                <button id="pvp-btn" className="big-yellow-btn heading-s" onClick={gameStartStop}>
+                  <span>player vs player</span>
+                  <img 
+                    src={pvpImg}
+                    alt="Player vs player"
+                  />
+                  </button>
+                <button id="rules-btn" className="big-white-btn heading-s" onClick={rulesClick}>game rules</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Fragment>
   )
 }
 
---
+++ src/components/RulesModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const rulesModal = ({rulesClick}) => {
+  return (
+    <div id='rules-modal'>
+      <div id='rules-cont'>
+          <h1 id='rules-heading' className='heading-l'>RULES</h1>
+          <h3 id="rules-obj-head" className='heading-s'>OBJECTIVE</h3>
+          <p id='rules-obj'>Be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).</p>
+          <h1 id="how-to-play-head" className='heading-s'>HOW TO PLAY</h1>
+          <ol id='how-to-play'>
+            <li>Red goes first in the game.</li>
+            <li>Players must alternate turns, and only one disk can be dropped in each turn.</li>
+            <li>The game ends when there is a 4-in-a-row or a stalemate.</li>
+            <li>The starter of the previous game goes second on the next game.</li>
+          </ol>
+          <button id='check-btn' onClick={rulesClick}>HEYYYYY!</button>
+      </div>
+    </div>
+  );
+}
+
+export default rulesModal;
